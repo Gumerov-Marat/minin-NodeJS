0869e6196f87b8c2bf20ce8ea2b977cd7546e058
Marat Gumerov <gumerovm74@gmail.com>
2020-10-28 15:00:09 +0300

    express 13

--- routes/courses.js
+++ routes/courses.js
@@ -4,12 +4,29 @@ const router = Router()
 
 router.get('/', async (req, res) => {
   const courses = await Course.getAll()
-  
   res.render('courses', {
     title: 'Курсы',
     isCourses: true,
     courses
   })
+})
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+
+  const course = await Course.getById(req.params.id)
+
+  res.render('course-edit', {
+    title: `Редактировать ${course.title}`,
+    course
+  })
+})
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
 })
 
 router.get('/:id', async (req, res) => {
